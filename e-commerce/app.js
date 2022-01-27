ddb054f2b209287f2a7321cee6b025e7e39dd8b3
Christopher Ozurumba <67044030+Mamiololo01@users.noreply.github.com>
2022-01-27 22:51:51 +0100

    READING ENVIRONMENTAL VARIABLES
    .env file was created with API_URL = /api/v1 and on running npm start, it printed '/api/v1'

--- e-commerce/app.js
+++ e-commerce/app.js
@@ -16,3 +16,20 @@ app.get('/', (req, res) => {
 app.listen(3000, ()=>{
     console.log('server is running http://localhost:3000');
 })
+
+// READING ENVIRONMENTAL VARIABLES //
+const express = require('express');
+const app = express();
+
+require('dotenv/config');
+
+const api = process.env.API_URL;
+
+app.get('/', (req, res) => {
+    res.send('hello API !');
+})
+
+app.listen(3000, ()=>{
+    console.log(api);
+    console.log('server is running http://localhost:3000');
+})
